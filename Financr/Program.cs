55e792da0a9d3da9e85c7d4111a73595afb3db07
Dave Whiteford <87719153+davewhiteford@users.noreply.github.com>
2023-02-05 13:40:49 +0000

    improved graphing in prep for maybe swapping in a better lib

--- Financr/Program.cs
+++ Financr/Program.cs
@@ -11,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<MortgageCalculator>();
+builder.Services.AddScoped<MortgageGrapher>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
---
+++ Financr/Utils/MortgageGrapher.cs
@@ -0,0 +1,71 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using MudBlazor;
+
+namespace Financr.Utils
+{
+    public class MortgageGrapher
+    {
+        public MortgageCalculator MortgageCalculator { get; protected set; }
+
+        public MortgageGrapher(MortgageCalculator mortgageCalculator)
+        {
+            MortgageCalculator = mortgageCalculator;
+        }
+
+        public List<ChartSeries> Series => new List<ChartSeries>()
+        {
+            new ChartSeries() { Name = "Balance", Data = this.BuildBalanceSeries() },
+            new ChartSeries() { Name = "Interest", Data = this.BuildDebtSeries() },
+            new ChartSeries() { Name = "Payment", Data = this.BuildPaymentSeries() },
+        };
+
+        public string[] XAxisLabels => this.BuildXAxisLabels();
+
+        public ChartOptions Options => new ChartOptions()
+        {
+            YAxisTicks = 10000,
+            MaxNumYAxisTicks = 10,
+            YAxisLines = true,
+            XAxisLines = true,
+        };
+
+        private double[] BuildBalanceSeries()
+        {
+            List<decimal> balance = new List<decimal> { this.MortgageCalculator.MortgageAmount };
+            balance.AddRange(this.MortgageCalculator.AmortizationSchedule.YearlyStatements.Select(x => x.EndingBalance));
+
+            return Array.ConvertAll(balance.ToArray(), x => (double)x);
+        }
+
+        private double[] BuildDebtSeries()
+        {
+            IList<decimal> debtPaid = new List<decimal>();
+            decimal runningTotalDebt = 0;
+            debtPaid.Add(runningTotalDebt);
+            foreach (var yearlyStatement in this.MortgageCalculator.AmortizationSchedule.YearlyStatements)
+            {
+                runningTotalDebt += yearlyStatement.Interest;
+                debtPaid.Add(runningTotalDebt);
+            }
+            return Array.ConvertAll(debtPaid.ToArray(), x => (double)x);
+        }
+
+        private double[] BuildPaymentSeries()
+        {
+            IList<decimal> totalPayments = new List<decimal>();
+            decimal runningTotalPayment = 0;
+            totalPayments.Add(runningTotalPayment);
+            foreach (var yearlyStatement in this.MortgageCalculator.AmortizationSchedule.YearlyStatements)
+            {
+                runningTotalPayment += MortgageCalculator.MonthlyPayment * 12;
+                totalPayments.Add(runningTotalPayment);
+            }
+            return Array.ConvertAll(totalPayments.ToArray(), x => (double)x);
+        }
+
+        private string[] BuildXAxisLabels()
+        {
+            return Array.ConvertAll(this.MortgageCalculator.AmortizationSchedule.YearlyStatements.Select(x => x.Period).ToArray(), x => x.ToString());
+        }
+    }
+}
