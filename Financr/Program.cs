8e339dae4f72970015f73f2c1c863af554f26d4e
Dave Whiteford <87719153+davewhiteford@users.noreply.github.com>
2023-02-08 21:12:19 +0000

    experimenting with plotly

--- Financr/Program.cs
+++ Financr/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Financr;
 using Financr.Utils;
 using MudBlazor.Services;
-using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,6 +11,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<MortgageCalculator>();
 builder.Services.AddScoped<MortgageGrapher>();
+builder.Services.AddScoped<PlotlyGrapher>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
+
--- Financr/Utils/PlotlyGrapher.cs
+++ Financr/Utils/PlotlyGrapher.cs
@@ -1,8 +1,33 @@
-﻿namespace Financr.Utils
+﻿using Plotly.Blazor;
+using Plotly.Blazor.Traces;
+using Plotly.Blazor.Traces.ScatterLib;
+
+namespace Financr.Utils
 {
     public class PlotlyGrapher
     {
+        public PlotlyGrapher()
+        {
+            Chart = new PlotlyChart();
+            this.Data = new List<ITrace>();
+        }
 
+        public PlotlyChart Chart;
+        public Config Config = new Config();
+        public Layout Layout = new Layout();
+        // Using of the interface IList is important for the event callback!
+        public IList<ITrace> Data { get; protected set; }
 
+        public async Task BuildChart()
+        {
+            this.Data.Clear();
+            List<object> foo = quote.Select(x => x.Open).Cast<object>().ToList();
+            this.Data.Add(new Scatter
+            {
+                Name = "ScatterTrace",
+                Mode = ModeFlag.Lines | ModeFlag.Markers,
+                Y = foo
+            });
+        }
     }
 }
