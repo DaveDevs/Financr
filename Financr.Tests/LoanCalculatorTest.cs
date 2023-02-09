485bc6a4de5cd85a1fff541c3183746edd26cc33
Dave Whiteford <87719153+davewhiteford@users.noreply.github.com>
2023-02-09 13:25:40 +0000

    Two graphing types coexist now.
    Test fixed.

--- Financr.Tests/LoanCalculatorTest.cs
+++ Financr.Tests/LoanCalculatorTest.cs
@@ -11,82 +11,87 @@ namespace Financr.Tests
         public void Calculate_Low()
         {
             // A
-            var mortgageCalc = new LoanCalculator();
-            mortgageCalc.PurchasePrice = 170_000;
-            mortgageCalc.Deposit = 10_000;
-            mortgageCalc.InterestRate = 4.65m;
-            mortgageCalc.Years = 25;
+            var loanCalc = new LoanCalculator();
+            loanCalc.PurchasePrice = 170_000;
+            loanCalc.Deposit = 10_000;
+            loanCalc.InterestRate = 4.65m;
+            loanCalc.Years = 25;
+            loanCalc.IsMortgage = true;
 
             // A
 
             // A
-            mortgageCalc.Lbtt.Should().Be(500.00m);
+            loanCalc.Lbtt.Should().Be(500.00m);
         }
 
         [Test]
         public void Calculate_Med()
         {
             // A
-            var mortgageCalc = new LoanCalculator();
-            mortgageCalc.PurchasePrice = 310_000;
-            mortgageCalc.Deposit = 10_000;
-            mortgageCalc.InterestRate = 4.65m;
-            mortgageCalc.Years = 25;
+            var loanCalc = new LoanCalculator();
+            loanCalc.PurchasePrice = 310_000;
+            loanCalc.Deposit = 10_000;
+            loanCalc.InterestRate = 4.65m;
+            loanCalc.Years = 25;
+            loanCalc.IsMortgage = true;
 
             // A
 
             // A
-            mortgageCalc.Lbtt.Should().Be(5100);
+            loanCalc.Lbtt.Should().Be(5100);
         }
 
         [Test]
         public void Calculate_High()
         {
             // A
-            var mortgageCalc = new LoanCalculator();
-            mortgageCalc.PurchasePrice = 510_000;
-            mortgageCalc.Deposit = 10_000;
-            mortgageCalc.InterestRate = 4.65m;
-            mortgageCalc.Years = 25;
+            var loanCalc = new LoanCalculator();
+            loanCalc.PurchasePrice = 510_000;
+            loanCalc.Deposit = 10_000;
+            loanCalc.InterestRate = 4.65m;
+            loanCalc.Years = 25;
+            loanCalc.IsMortgage = true;
 
             // A
 
             // A
-            mortgageCalc.Lbtt.Should().Be(24350.00m);
+            loanCalc.Lbtt.Should().Be(24350.00m);
         }
 
         [Test]
         public void Calculate_Monthly()
         {
             // A
-            var mortgageCalc = new LoanCalculator();
-            mortgageCalc.PurchasePrice = 510_000;
-            mortgageCalc.Deposit = 10_000;
-            mortgageCalc.InterestRate = 4.65m;
-            mortgageCalc.Years = 25;
+            var loanCalc = new LoanCalculator();
+            loanCalc.PurchasePrice = 510_000;
+            loanCalc.Deposit = 10_000;
+            loanCalc.InterestRate = 4.65m;
+            loanCalc.Years = 25;
+            loanCalc.IsMortgage = true;
 
             // A
 
             // A
-            mortgageCalc.MonthlyPayment.Should().BeApproximately(2821.91m, 0.01m);
+            loanCalc.MonthlyPayment.Should().BeApproximately(2821.91m, 0.01m);
         }
 
         [Test]
         public void Calculate_Amortization()
         {
             // A
-            var mortgageCalc = new LoanCalculator();
-            mortgageCalc.PurchasePrice = 200_000;
-            mortgageCalc.Deposit = 0;
-            mortgageCalc.InterestRate = 6;
-            mortgageCalc.Years = 25;
+            var loanCalc = new LoanCalculator();
+            loanCalc.PurchasePrice = 200_000;
+            loanCalc.Deposit = 0;
+            loanCalc.InterestRate = 6;
+            loanCalc.Years = 25;
+            loanCalc.IsMortgage = true;
 
             // A
-            var schedule = mortgageCalc.CalculateAmortization();
+            var schedule = loanCalc.CalculateAmortization();
 
             // A
-            schedule.MonthlyStatements.Count.Should().Be(mortgageCalc.Years * 12);
-            schedule.YearlyStatements.Count.Should().Be(mortgageCalc.Years);
+            schedule.MonthlyStatements.Count.Should().Be(loanCalc.Years * 12);
+            schedule.YearlyStatements.Count.Should().Be(loanCalc.Years);
 
             schedule.MonthlyStatements.Last().EndingBalance.Should().BeApproximately(0m, 0.0001m);
             
